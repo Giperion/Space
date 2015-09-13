4fb31585285f3808da042e6f5c0dc172fae45081
Giperion <dereline@hotmail.com>
2015-09-13 23:59:20 +0300

    Added Generator.cs

---
+++ Assets/SpaceGame/Scripts/Generator/Generator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+    public GameObject[] ForestChunks;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

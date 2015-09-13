90f055bb9d0558920251788f22e782a8df3326ee
gerlord <amil0512@gmail.com>
2015-09-14 02:28:21 +0500

    Мама ама криминал

--- Assets/SpaceGame/Scripts/Generator/Generator.cs
+++ Assets/SpaceGame/Scripts/Generator/Generator.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class Generator : MonoBehaviour {
 
     public GameObject[] ForestChunks;
+    public bool isCavern;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }

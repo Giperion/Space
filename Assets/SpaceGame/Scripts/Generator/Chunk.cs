28401bf8779cc2851265e538d510e0501e2faab6
gerlord <amil0512@gmail.com>
2015-09-16 16:35:03 +0500

    Обновление V0.0.3

--- Assets/SpaceGame/Scripts/Generator/Chunk.cs
+++ Assets/SpaceGame/Scripts/Generator/Chunk.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 
 public class Chunk : MonoBehaviour {
 
-    public GameObject[] chunkObj;
-    public List<GameObject> PrefabsForGenerick;
+    public GameObject[] PrefabsForGenerick;
     public int typeOfChunk = 0; // 0 - лес, 1 - переход лес/пещера, 2 - пещера, 3 - переход пещера лес
 
     void Start ()
     {
-	    // т.к. скрипт будет весеть на префабе чанка, то будет генерить на себе мусор относительно его типа
-	}
+        if (typeOfChunk == 0)
+        {
+            CreateForestChunk();
+        }
+        // т.к. скрипт будет весеть на префабе чанка, то будет генерить на себе мусор относительно его типа
+    }
+    void CreateForestChunk()
+    {
+        Instantiate(PrefabsForGenerick[0], this.transform.localPosition, Quaternion.identity);
+    }
 	
 	void Update ()
     {
--- Assets/SpaceGame/Scripts/Generator/Generator.cs
+++ Assets/SpaceGame/Scripts/Generator/Generator.cs
@@ -1,50 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Generator : MonoBehaviour {
 
     public GameObject[] M_Chunks; // 0 - лес, 1 - переход лес/пещера, 2 - пещера, 3 - переход пещера лес 
+    public List<GameObject> chunksOnScene;
+    
+    GameObject PlayerOnScene;
 
     int zNotePosCursor = 0;
     int numberOfLastChunk = 0;
-    int typeOfLastChunk = 0; // 0 - лес, 1 - переход лес/пещера, 2 - пещера, 3 - переход пещера лес 
+    int typeOfLastChunk = 0; // 0 - лес, 1 - переход лес/пещера, 2 - пещера, 3 - переход пещера/лес 
+
+    bool needToReedNow = true;
 
     void Start ()
     {
-        StartCoroutine(callPositionPlayer());
+        PlayerOnScene = GameObject.FindGameObjectWithTag("Player");
     }
 
 	void Update ()
-    {  
-          
+    {
+        if (needToReedNow)
+            StartCoroutine(callPositionPlayer());
     }
 
     IEnumerator callPositionPlayer()
     {
+        needToReedNow = false;
+        AddCunk();
         yield return new WaitForSeconds(2);
-        AddCunk();      
+        needToReedNow = true;
     }
 
     void AddCunk()
     {
-        // Добавить условие на нужду создание этого чанка по времени
         if (numberOfLastChunk == 0)
         {
-           Instantiate(M_Chunks[0], new Vector3(0, 0, zNotePosCursor), Quaternion.identity);    
-        }
-        else if (numberOfLastChunk != 0)
-        {
-            if (typeOfLastChunk == 0)
+            while (chunksOnScene.Count < 3)
             {
-                //рандомим тип след чанка
-            }
+                CreateChunk();
+            }  
         }
-
-        zNotePosCursor += 10;
-        numberOfLastChunk++;
+        else if (numberOfLastChunk != 0 && chunksOnScene.Count < 12)
+        {           
+            CreateChunk();  
+        }  
     }
-    void RemoveChunk()
-    {
 
+    void CreateChunk(int typeOf = 0)
+    {
+        GameObject bufferCunk = Instantiate(M_Chunks[WhichChunkNeed()], new Vector3(0, 0, zNotePosCursor), Quaternion.identity) as GameObject; // нужно как то доаблять их сразу в лист chunksOnScene
+        chunksOnScene.Add(bufferCunk);
+        IfAddChunk();
+    }
+
+    void IfAddChunk()
+    {
+        zNotePosCursor += 30;
+        numberOfLastChunk++;
+        RemoveOldChunk();
+    }
+
+    int WhichChunkNeed()
+    {
+        if (typeOfLastChunk == 0)
+        {
+            typeOfLastChunk = Random.Range(0, 2);
+        }
+        else if (typeOfLastChunk == 1)
+        {
+            typeOfLastChunk = 2;
+        }
+        else if (typeOfLastChunk == 2)
+        {
+            typeOfLastChunk = Random.Range(2, 4);
+        }
+        else if (typeOfLastChunk == 3)
+        {
+            typeOfLastChunk = 0;
+        }
+        return typeOfLastChunk;
+    }
+
+    void RemoveOldChunk()
+    {
+        if (chunksOnScene.Count >= 12)
+        {
+            
+        }
     }
 }
